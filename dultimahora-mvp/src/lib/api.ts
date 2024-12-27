91d3a16da8ab16b64685a7f3dc00582bf8b54b27
Lucas Borges da Silva <lucas18sborges@gmail.com>
2024-12-27 01:27:54 -0300

    teste de remoção

--- dultimahora-mvp/src/lib/api.ts
+++ dultimahora-mvp/src/lib/api.ts
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 // on .env file, exists a variable called API_LINK, that is the link to the API
 
-const API_LINK = process.env.API_LINK;
 
 export const api = axios.create({
-  baseURL: `${API_LINK}/api`,
+  baseURL: `/api`,
 });
