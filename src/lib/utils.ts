716cc00106939c75b09bae14e4e78fea0d249869
Lucas Borges <lucas18sborges@gmail.com>
2025-01-27 02:26:31 -0300

    ADD: Adicionando função formatDateString para formatação de datas e atualizando a exibição na página de evento

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDateString(date: Date) {
+  return date.toString().split('T')[0].split('-').reverse().join('/')
+}
+
+export function smallDateString(date: Date) {
+  const formatter = new Intl.DateTimeFormat('pt-BR', { month: 'short', day: 'numeric' });
+
+  const fixed_date = new Date(date).setDate(new Date(date).getDate() + 1)
+
+  return `${formatter.format(fixed_date)}`
+}
