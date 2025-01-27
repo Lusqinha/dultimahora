fafbd0c0e6af206962c13e427144ac07e7416862
Lucas Borges <lucas18sborges@gmail.com>
2025-01-27 02:25:49 -0300

    ADD: Adicionando ícone de ingressos e ajustando o componente EventCard

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,16 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
-import { type Evento } from "@prisma/client"
+import { type Evento as PrismaEvento } from "@prisma/client"
+
+interface Evento extends PrismaEvento {
+  _count: {
+    ingressos: number;
+  };
+}
 import Link from "next/link"
 
+import { smallDateString } from "@/lib/utils"
+import Image from "next/image"
+
 import { Card, CardContent } from "@/components/ui/card"
-import { Switch } from "@/components/ui/switch"
+import { CalendarDaysIcon, TicketIcon, TicketsIcon } from "lucide-react"
 
 
-
-export default function EventCard({ id, nome, banner_path }: Evento) {
+export default function EventCard({ id, nome, date, banner_path, _count }: Evento) {
   return (
     <Link href={`/evento/${id}`}>
-      <Card className="overflow-hidden bg-white border-[#FBC004] border hover:shadow-lg transition-shadow">
+      <Card className="overflow-hidden bg-white hover:shadow-lg transition-shadow mx-auto">
         <CardContent className="p-0">
           <div className="relative">
             <img 
@@ -18,18 +26,19 @@ export default function EventCard({ id, nome, banner_path }: Evento) {
               alt={nome}
               className="w-full h-[200px] object-cover bg-black/40"
             />
-            <div className="absolute top-0 left-0 bg-[#FBC004] px-3 py-1">
-              <h3 className="text-black font-medium">{ nome }</h3>
+            <div className=" text-xl text-[#2248FF] flex items-center absolute top-0 right-0 bg-white rounded-bl-xl px-3 py-1">
+              <CalendarDaysIcon className="mr-2" size={24} />
+              <h3 className="text-black font-medium">{ smallDateString(date) }</h3>
             </div>
           </div>
-          <div className="p-4 flex items-center justify-between">
-            <span className="text-[#3F7EA7] text-sm font-medium">
-              { true ? 'Ingressos Disponíveis' : 'Esgotado'}
-            </span>
-            <Switch 
-              checked={true}
-              className="data-[state=checked]:bg-[#3F7EA7]"
-            />
+          <div className="p-4 flex text-white items-center justify-between">
+            <h2 className="text-lg font-bold text-[#2248FF]">{nome.toLocaleUpperCase()}</h2>
+            <p className="bg-[#FFC006] flex items-center font-extrabold text-1xl ml-2 py-1 px-3 gap-1 rounded-full" >
+              <span>{_count.ingressos}</span>
+              <Image src="/img/ticket-event-card.svg" alt="Ingressos" width={30} height={30} />
+              
+
+            </p>
           </div>
         </CardContent>
       </Card>
