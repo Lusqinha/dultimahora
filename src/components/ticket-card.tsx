85418bda471f6f155fed6e8befaa7d4ea6639715
Lucas Borges <lucas18sborges@gmail.com>
2025-01-29 01:15:58 -0300

    FEAT: Adiciona novos ícones de ingresso e atualiza o design do cartão de ingresso

--- src/components/ticket-card.tsx
+++ src/components/ticket-card.tsx
@@ -1,37 +1,40 @@
 import Link from "next/link"
 import { Card, CardContent } from "@/components/ui/card"
-import { ChevronRightIcon } from 'lucide-react'
-import { Ingresso } from "@prisma/client"
+import Image from "next/image"
+import type { Ingresso } from "@prisma/client"
 
 export function TicketCard({ id, formato_ingresso, qtd_ingressos, tipo_ingresso, nome_completo, valor_un }: Ingresso) {
   return (
     <Link href={`/ingresso/${id}`}>
-      <Card className="bg-white hover:bg-stone-800 hover:text-[#FBC004]  ease-in-out transition-all shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] m-3 sm:m-auto max-w-[400px] mx-auto rounded-[50px] rounded-ss-none rounded-br-none ">
-        <CardContent className=" mx-5 p-4">
-          <div className="flex items-center justify-between">
-            <div className="space-y-2">
-              <p className=" font-bold">
-                {nome_completo} ofereceu:
-              </p>
-              <div className="flex items-center gap-2">
-                <span className="font-semibold">
-                  {qtd_ingressos} {qtd_ingressos > 1 ? 'Entradas' : 'Entrada'}
-                </span>
-                <span>-</span>
-                <span className="font-semibold">
-                  R$ {valor_un.toFixed(2)}
-                </span>
-              </div>
-              <div className="flex gap-4 text-sm text-muted-foreground">
-                <div>
-                  Form: <span className="text-foreground">{formato_ingresso}</span>
-                </div>
-                <div>
-                  Tipo: <span className="text-foreground">{tipo_ingresso}</span>
-                </div>
-              </div>
+      <Card className="bg-white hover:bg-gray-50 min-h-full transition-all duration-300 shadow-md m-3 sm:m-auto min-w-[318px] max-w-[400px] mx-auto rounded-xl overflow-hidden border border-gray-200 flex flex-col justify-center cursor-pointer"> 
+        <CardContent className="p-6 min-h-full">
+          <div className="flex items-center justify-between mb-4">
+            <div className="flex items-center">
+              <Image src={'/img/tickets/ticket-azul.svg'} width={14} height={14} alt="ticket azul" className="mr-2" />
+              <span className="font-semibold text-lg text-gray-800 text-pretty">{nome_completo.split(' ')[0].split('', 9).reduce((o, c) => o.length === 8 ? `${o}${c}...` : `${o}${c}`, '')}</span>
             </div>
-            <ChevronRightIcon className="h-7 w-7 text-[#FBC004] font-extrabold" />
+            <span className="text-2xl font-bold text-[#2248FF]">R${valor_un.toFixed(2)}</span>
+          </div>
+          <div className="space-y-2">
+            <div className="flex justify-between items-center">
+              <span className="text-gray-600">Quantidade:</span>
+              <span className="font-medium text-gray-800">
+                {qtd_ingressos} {qtd_ingressos > 1 ? "Entradas" : "Entrada"}
+              </span>
+            </div>
+            <div className="flex justify-between items-center">
+              <span className="text-gray-600">Formato:</span>
+              <span className="font-medium text-gray-800">{formato_ingresso}</span>
+            </div>
+            <div className="flex justify-between items-center">
+              <span className="text-gray-600">Tipo:</span>
+              <span className="font-medium text-gray-800">{tipo_ingresso}</span>
+            </div>
+          </div>
+          <div className="mt-4 pt-4 border-t self-end border-gray-200">
+            <button className="w-full bg-[#FFC006] text-white py-2 px-4 rounded-lg hover:bg-[#bf9d38] transition-colors duration-300">
+              Ver D'etalhes
+            </button>
           </div>
         </CardContent>
       </Card>
