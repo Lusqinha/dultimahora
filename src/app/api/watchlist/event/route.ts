9da8ebbca311100d8f5aa9fad36c1c3844230e94
Lucas Borges <lucas18sborges@gmail.com>
2025-02-04 04:03:15 -0300

    FEAT: Adiciona verificação de duplicidade na lista de notificações para eventos e palavras-chave

--- src/app/api/watchlist/event/route.ts
+++ src/app/api/watchlist/event/route.ts
@@ -5,11 +5,22 @@ export async function POST(req: NextRequest) {
 
     const data = await req.json();
 
+    const checkWatchlist = await prisma.watchlist.findFirst({
+        where: {
+            eventoId: data.eventoId,
+            contato_whatsapp: data.contato_whatsapp
+        }
+    });
+
+    if (checkWatchlist) {
+        return NextResponse.json({ message: "Você já está na lista de notificação deste evento." }, { status: 400 });
+    }
+
     const watchlist = await prisma.watchlist.create({
         data: {
+            eventoId: data.eventoId,
             nome_completo: data.nome_completo,
-            contato_whatsapp: data.contato_whatsapp,
-            eventoId: data.eventoId
+            contato_whatsapp: data.contato_whatsapp
         }
     });
     
--- src/app/api/watchlist/keyword/route.ts
+++ src/app/api/watchlist/keyword/route.ts
@@ -5,6 +5,17 @@ export async function POST(req: NextRequest) {
 
     const data = await req.json();
 
+    const checkWatchlist = await prisma.watchlist.findFirst({
+        where: {
+            keyword: data.keyword.toLowerCase(),
+            contato_whatsapp: data.contato_whatsapp
+        }
+    });
+
+    if (checkWatchlist) {
+        return NextResponse.json({ message: "Você já está na lista de notificação para esta palavra-chave." }, { status: 400 });
+    }
+
     const watchlist = await prisma.watchlist.create({
         data: {
             nome_completo: data.nome_completo,
