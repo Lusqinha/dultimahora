3271c3b24fd348feb81e529cd2b3f3881d2ab157
Lucas Borges <lucas18sborges@gmail.com>
2025-02-06 15:36:17 -0300

    FEAT: Melhora o layout da seção "Fale Conosco" com ajustes de estilo e tamanho

--- src/components/home/contact-us.tsx
+++ src/components/home/contact-us.tsx
@@ -11,17 +11,17 @@ export function ContactUs({ className }: { className?: string }) {
         <section className={cn("bg-[#2248FF] py-16 rounded-lg", className)}>
             <div className="container mx-auto px-4">
                 <div className="max-w-3xl mx-auto text-center">
-                    <h2 className="text-3xl font-bold text-white mb-4">Fale Conosco!</h2>
-                    <p className="text-white text-lg mb-8">
+                    <h2 className="text-4xl font-bold text-white mb-4">Fale Conosco!</h2>
+                    <p className="text-white mb-8">
                         Tem alguma dúvida, sugestão ou feedback? Estamos aqui para ajudar! Entre em contato conosco pelo WhatsApp e
                         teremos prazer em atendê-lo.
                     </p>
                     <Button
                         onClick={handleWhatsAppClick}
-                        className="bg-[#FFC006] text-white font-bold hover:bg-[#FFC006]/90 text-lg px-8 py-3 rounded-full flex items-center justify-center mx-auto"
+                        className="bg-[#FFC006] text-white font-bold hover:bg-[#FFC006]/90 text-xl px-8 py-6 rounded-2xl flex items-center justify-center mx-auto"
                     >
-                        <MessageCircle className="mr-1 h-8 w-8" />
-                        Falar no WhatsApp
+                        <MessageCircle strokeWidth={3} />
+                        Falar no WhatsApp      
                     </Button>
                 </div>
             </div>
