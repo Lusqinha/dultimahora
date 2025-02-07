210e70dc3993877b56f8634e6d615ff7d510cea1
Lucas Borges <lucas18sborges@gmail.com>
2025-02-07 00:17:13 -0300

    STYLE: Altera o alinhamento do título do evento para centralizado em telas médias e maiores

--- src/app/evento/[id]/page.tsx
+++ src/app/evento/[id]/page.tsx
@@ -45,7 +45,7 @@ export default function EventPage() {
           <div className="relative w-full h-64 sm:h-80 md:h-96">
             <Image src={banner || "img/dultimahora-evento-placeholder.png"} alt={event.nome} layout="fill" objectFit="cover" priority />
             <div className="absolute inset-0 bg-black bg-opacity-50 flex items-start p-4 sm:p-6 flex-col justify-end">
-              <h1 className="text-2xl text-center sm:text-3xl md:text-4xl font-bold text-white">{event.nome}</h1>
+              <h1 className="text-2xl md:text-center sm:text-3xl md:text-4xl font-bold text-white mr-10">{event.nome}</h1>
               <h1 className="text-lg text-center sm:text-xl md:text-2xl font-bold text-white flex gap-2">
                 <CalendarDaysIcon />
                 {formatDateString(event.date)}
