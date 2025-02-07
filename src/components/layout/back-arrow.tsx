f93aaddac1dc92ab2b6495c6f1a884b4fdcd0bbc
Lucas Borges <lucas18sborges@gmail.com>
2025-02-06 22:48:37 -0300

    STYLE: Altera a cor de fundo do ícone de seta para a esquerda no componente BackArrow

--- src/components/layout/back-arrow.tsx
+++ src/components/layout/back-arrow.tsx
@@ -5,7 +5,7 @@ export function BackArrow() {
     return (
         <div className="md:hidden fixed top-4 left-4 z-50">
             <button onClick={() => window.history.back()} aria-label="Go back">
-                <ChevronLeftIcon className="w-10 h-10 text-white bg-[#2248FF]/10 rounded-full p-2 " />
+                <ChevronLeftIcon className="w-10 h-10 text-white bg-[#FFC006]/10 rounded-full p-2 " />
             </button>
         </div>
     )
