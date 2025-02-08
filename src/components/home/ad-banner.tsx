dfcdaee621c7391371b39cf5a5810dfc727c133f
Lucas Borges da Silva <lucas18sborges@gmail.com>
2025-02-08 18:44:53 -0300

    FEAT: Adiciona link para compra de espaço publicitário no banner de anúncio

--- src/components/home/ad-banner.tsx
+++ src/components/home/ad-banner.tsx
@@ -18,7 +18,9 @@ export function AdBanner({ className }: { className?: string }) {
             <DollarSignIcon className="w-12 h-12 mx-auto mb-4 text-white" />
             <h3 className="text-lg lg:text-xl font-bold text-white mb-2">Espaço Publicitário Disponível</h3>
             <p className="text-white text-xs lg:text-lg mb-4">Alcance seu público-alvo neste evento!</p>
-            <button className="bg-white text-[#FFC006] text-md font-bold py-2 px-4 rounded hover:bg-opacity-90 transition duration-300">
+            <button className="bg-white text-[#FFC006] text-md font-bold py-2 px-4 rounded hover:bg-opacity-90 transition duration-300"
+                onClick={() => window.open("https://wa.me/5555996065533", "_blank")}
+            >
                 Comprar Espaço
             </button>
         </div>
