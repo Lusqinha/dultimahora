95e2ef8c1b4bbc1728646eacb0cd295b5eb3b924
Lucas Borges da Silva <lucas18sborges@gmail.com>
2025-02-08 18:45:07 -0300

    FEAT: Adiciona JSON-LD para estrutura de dados do evento

--- src/app/evento/[id]/page.tsx
+++ src/app/evento/[id]/page.tsx
@@ -36,54 +36,80 @@ export default function EventPage() {
   }, [event_id])
 
   const banner = event?.banner_path || "/banners/dultimahora-evento-placeholder.png"
+  let jsonLd = null
+  if (event) {
+    // JSON-LD structured data
+      jsonLd = {
+      "@context": "https://schema.org",
+      "@type": "Event",
+      name: event.nome,
+      startDate: event.date,
+      image: banner,
+      description: `Encontre ingressos para ${event.nome} no D'Ultima Hora.`,
+      offers: {
+        "@type": "AggregateOffer",
+        availability: "https://schema.org/InStock",
+        priceCurrency: "BRL",
+        offerCount: event.ingressos.length,
+      },
+      organizer: {
+        "@type": "Organization",
+        name: "D'Ultima Hora",
+        url: "https://dultimahora.fluxstudio.com.br/",
+      },
+    }
+  }
 
   return (
-    <div className="min-h-screen bg-white lg:w-2/3 mx-auto">
-      <BackArrow />
-      {event && (
-        <>
-          <div className="relative w-full h-64 sm:h-80 md:h-96">
-            <Image src={banner || "img/dultimahora-evento-placeholder.png"} alt={event.nome} layout="fill" objectFit="cover" priority />
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-start p-4 sm:p-6 flex-col justify-end">
-              <h1 className="text-2xl md:text-center sm:text-3xl md:text-4xl font-bold text-white mr-10">{event.nome}</h1>
-              <h1 className="text-lg text-center sm:text-xl md:text-2xl font-bold text-white flex gap-2">
-                <CalendarDaysIcon />
-                {formatDateString(event.date)}
-              </h1>
+    <>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+      <div className="min-h-screen bg-white lg:w-2/3 mx-auto">
+        <BackArrow />
+        {event && (
+          <>
+            <div className="relative w-full h-64 sm:h-80 md:h-96">
+              <Image src={banner || "img/dultimahora-evento-placeholder.png"} alt={event.nome} layout="fill" objectFit="cover" priority />
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex items-start p-4 sm:p-6 flex-col justify-end">
+                <h1 className="text-2xl md:text-center sm:text-3xl md:text-4xl font-bold text-white mr-10">{event.nome}</h1>
+                <h1 className="text-lg text-center sm:text-xl md:text-2xl font-bold text-white flex gap-2">
+                  <CalendarDaysIcon />
+                  {formatDateString(event.date)}
+                </h1>
+              </div>
+              <div className="absolute bottom-7 right-8 scale-125">
+                <NotificationModal eventoId={event.id} eventName={event.nome} />
+              </div>
             </div>
-            <div className="absolute bottom-7 right-8 scale-125">
-              <NotificationModal eventoId={event.id} eventName={event.nome} />
+            
+            <div className="px-4 py-6 space-y-6">
+
+              <AdBanner />
+
+              <div>
+                <h2 className="text-2xl font-bold text-[#2248FF] mb-4 flex text-center justify-center items-center">
+                  <TicketsIcon className="mr-2" />
+                  Ingressos anunciados
+                </h2>
+                <TicketList tickets={event.ingressos} />
+              </div>
             </div>
-          </div>
-          
-          <div className="px-4 py-6 space-y-6">
+            <div className="my-12 w-3/4 mx-auto text-center">
+              <p className="text-xl text-gray-700 mb-4">
+                <span>
+                  Quer ficar de olho aqui?👀
 
-            <AdBanner />
-
-            <div>
-              <h2 className="text-2xl font-bold text-[#2248FF] mb-4 flex text-center justify-center items-center">
-                <TicketsIcon className="mr-2" />
-                Ingressos anunciados
-              </h2>
-              <TicketList tickets={event.ingressos} />
+                </span>
+                <br />
+                <span className="text-gray-500 text-xs">
+                  Receba um aviso no <u>WhatsApp</u> sempre que<br /> anunciarem neste evento!
+                </span>
+              </p>
+              <NotificationModal eventoId={event.id} eventName={event.nome} btnType="full" />
             </div>
-          </div>
-          <div className="my-12 w-3/4 mx-auto text-center">
-            <p className="text-xl text-gray-700 mb-4">
-              <span>
-                Quer ficar de olho aqui?👀
-
-              </span>
-              <br />
-              <span className="text-gray-500 text-xs">
-                Receba um aviso no <u>WhatsApp</u> sempre que<br /> anunciarem neste evento!
-              </span>
-            </p>
-            <NotificationModal eventoId={event.id} eventName={event.nome} btnType="full" />
-          </div>
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </div>
+    </>
   )
 }
 
