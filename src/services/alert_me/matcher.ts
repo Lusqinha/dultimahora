1ee877f559ce5d9a82b6136dbf24a52b9bf5ae70
Lucas Borges <lucas18sborges@gmail.com>
2025-03-02 01:00:01 -0300

    FEAT: Adiciona espaço em branco à lista de palavras ignoradas no matcher

--- src/services/alert_me/matcher.ts
+++ src/services/alert_me/matcher.ts
@@ -4,7 +4,7 @@ import type { Watchlist } from "@prisma/client";
 import prisma from "@/lib/db";
 
 const ignoreWords = [
-    'a', 'o', 'e', 'é', 'de', 'do', 'da', 'em', 'para', 'por', 'com', 'sem', 'que', 'na', 'no', 'as', 'os', 'um', 'uma', 'uns', 'umas', 'ao', 'aos', 'à', 'às', 'pelo', 'pela', 'pelos', 'pelas'
+    'a', 'o', 'e', 'é', 'de', 'do', 'da', 'em', 'para', 'por', 'com', 'sem', 'que', 'na', 'no', 'as', 'os', 'um', 'uma', 'uns', 'umas', 'ao', 'aos', 'à', 'às', 'pelo', 'pela', 'pelos', 'pelas', ' '
 ];
 
 export interface CheckData {
